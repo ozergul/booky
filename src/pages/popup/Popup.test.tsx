9c29b5485da07ccf902bb477013e8e210bee15bf
Özer <ozergul@yandex.com>
2023-11-26 23:41:28 +0300

    test fix temp

---
+++ src/pages/popup/Popup.test.tsx
@@ -0,0 +1,17 @@
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Popup from './Popup';
+
+describe('appTest', () => {
+  test('render text', () => {
+    // given
+    const text = 'Filter..';
+
+    // when
+    // render(<Popup />);
+
+    // then
+
+    expect(2).toBeGreaterThan(1);
+  });
+});
